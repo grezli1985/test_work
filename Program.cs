c384e5e98262693831b2e032bf06c1e0a1bcb5a6
grezli <grezli@mail.ru>
2022-08-07 16:27:44 +0300

    Zadacha

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*  Задача: 
+
+Примеры:
+
+{"hello", "2", "world", ":-)"} -> ["2", ":-)"]
+
+{"1234", "1567", "-2", "computer science"} -> ["-2"]
+
+{"Russia", "Denmark", "Kazan"} -> [] */
+
+Zadacha();
+
+void Zadacha()
+{
+    string[]array = {"hello", "2", "world", ":-)"}; 
+    PrintArray(array);
+    Console.WriteLine();
+    SortArray(array);
+    Console.WriteLine();
+}
+
